753f8afd5e07bad0694e7829b4502ef101172109
Evgenii Lapotko <lapotko.e2@gmail.com>
2021-04-30 23:40:26 +0300

    task8

--- task8-React-Slider/src/App.jsx
+++ task8-React-Slider/src/App.jsx
@@ -1,34 +1,81 @@
-import React, {useState} from "react";
-import data from './data';
-import {FaChevronRight, FaChevronLeft, FaQuoteRight} from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import { FaChevronRight, FaChevronLeft, FaQuoteRight } from "react-icons/fa";
 
 const App = () => {
     const [people, setPeople] = useState(data);
     const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const lastIndex = people.length - 1;
+        if (index < 0) {
+            setIndex(lastIndex);
+        }
+        if (index > lastIndex) {
+            setIndex(0);
+        }
+    }, [index, people]);
+
+    useEffect(() => {
+        let slider = setInterval(() => {
+            setIndex(index + 1);
+        }, 2000);
+        return () => clearInterval(slider);
+    }, [index]);
     return (
         <main>
             <section className="slider">
-            <h2 className="title"><span>/</span>Reviews</h2>
-            <div className="slider__inner">
-                {people.map((person, personIndex) => {
-                    const {id, title, image, quote, name} = person;
+                <h2 className="title">
+                    <span>/</span>Reviews
+                </h2>
+                <div className="slider__inner">
+                    {people.map((person, personIndex) => {
+                        const { id, title, image, quote, name } = person;
+                        let position = "nextSlide";
+                        if (personIndex === index) {
+                            position = "activeSlide";
+                        }
+                        if (
+                            personIndex === index - 1 ||
+                            (index === 0 && personIndex === people.length - 1)
+                        ) {
+                            position = "lastSlide";
+                        }
 
-
-                    return <article className="slider__item" key={id}>
-                        <img src={image} alt={name} className="slider__item-img"/>
-                        <div className="slider__item-name">{name}</div>
-                        <div className="slider__item-title">{title}</div>
-                        <div className="slider__item-quote">{quote}</div>
-                        <FaQuoteRight className='slider__item-icon' />
-                    </article>
-                })}
-                <button className="prev">
-                    <FaChevronLeft />
-                </button>
-                <button className="next">
-                    <FaChevronRight />
-                </button>
-            </div>
+                        return (
+                            <article
+                                className={`slider__item ${position}`}
+                                key={id}
+                            >
+                                <img
+                                    src={image}
+                                    alt={name}
+                                    className="slider__item-img"
+                                />
+                                <div className="slider__item-name">{name}</div>
+                                <div className="slider__item-title">
+                                    {title}
+                                </div>
+                                <div className="slider__item-quote">
+                                    {quote}
+                                </div>
+                                <FaQuoteRight className="slider__item-icon" />
+                            </article>
+                        );
+                    })}
+                    <button
+                        className="prev"
+                        onClick={() => setIndex(index - 1)}
+                    >
+                        <FaChevronLeft />
+                    </button>
+                    <button
+                        className="next"
+                        onClick={() => setIndex(index + 1)}
+                    >
+                        <FaChevronRight />
+                    </button>
+                </div>
             </section>
         </main>
     );
