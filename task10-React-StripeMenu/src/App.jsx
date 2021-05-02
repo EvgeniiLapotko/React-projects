a735df8d014b1ff7297a2f6b674c85202cccbf55
Evgenii Lapotko <lapotko.e2@gmail.com>
2021-05-03 00:14:10 +0300

    task10-part1

---
+++ task10-React-StripeMenu/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <Sidebar />
+            <Hero />
+            <Submenu />
+        </>
+    );
+};
+
+export default App;
